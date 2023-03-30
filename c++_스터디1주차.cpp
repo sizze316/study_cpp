6871f5cd638afa9614f9db206577258d04d5b4f5
sizze316 <128690166+sizze316@users.noreply.github.com>
2023-03-31 01:57:54 +0900

    Add files via upload

--- c++_스터디1주차.cpp
+++ c++_스터디1주차.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main() {
 
 	char name[10];
-	char number[20];
+	char number[20]; 
 
 	cout << "이름: ";
-	cin.getline(name, 10);
+	cin.getline(name, 10); /*cin.getline은 공백을 포함한 문자열을 입력할 수 있음*/
 
 	cout << "학번: ";
 	cin.getline(number, 20);
 
-	cout << "반갑습니다." << name << "님.\n" << endl;
+	cout << "반갑습니다." << name << "님.\n" << endl; /*위에 cin에서 입력한 name을 받아옴*/
 
 	cout << "학점 입력을 시작합니다.\n" << endl;
 
 	cout << "========== SWING 31 ==========\n";
 	
-	double a, b, c, d;
+	double a, b, c, d; /*학점은 실수 이므로 double로 받아야함*/
 
 	cout << "C프로그래밍(3학점): ";
 	cin >> a;
@@ -39,7 +39,8 @@ int main() {
 	cout << "==============================\n";
 
 	cout << name  << "("<< number <<")" << "님의 전체 학점은 " << (((a * 3) + (b * 3) + (c * 1) + (d * 2)) / 9) << "입니다.\n" << endl;
-	
+	/*위에서 입력한 name과 number을 받아옴*/
+
 
 	return 0;
 }
