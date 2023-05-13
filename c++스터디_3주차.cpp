3318148dc46eb7b4469ba546984ec586cc1977b0
sizze316 <128690166+sizze316@users.noreply.github.com>
2023-05-13 18:36:53 +0900

    Add files via upload

--- c++스터디_3주차.cpp
+++ c++스터디_3주차.cpp
@@ -4,94 +4,125 @@
 
 using namespace std;
 
+void myaccount(string* bank_name, int* bank) {
+    cout << "-------------------------- swing 계좌 정보 --------------------------" << endl;
+    cout << "|    " << bank_name[0] << "   |    " << bank_name[1] << "   |    " << bank_name[2] << "   |    " << bank_name[3] << "   |" << endl;
+    cout << "|    " << bank[0] << "   |    " << bank[1] << "   |    " << bank[2] << "   |    " << bank[3] << "   |" << endl;
+    cout << "---------------------------------------------------------------------" << endl;
+    cout << endl;
+}
+
+int total(int* bank) {
+    int sum = 0;
+    for (int q = 0; q < 4; q++) {
+
+        sum += bank[q];
+    }
+    return sum;
+}
+
 int main(void) {
-	int a, b, pw, loginpw;
-	char name[10];
-	char id[20];
-	char loginid[30];
 
-	while (1) {
-		cout << " ********** swing 은행 **********" << endl;
-		cout << "1. 회원가입 " << endl;
-		cout << "2. 로그입" << endl;
-		cout << "번호를 입력하세요 >> ";
-		cin >> a;
+    int a, b, pw, loginpw;
+    char name[10];
+    char id[20];
+    char loginid[30];
 
-		if (a == 1) {
+    int is_logged_in = false; // 로그인 여부 확인하는 변수
 
-			cout << endl;
-			cout << "이름: " ;
-			cin >> name;
-			cout << "아이디: " ;
-			cin >> id;
-			cout << "비밀번호: " ;
-			cin >> pw;
+    string bank_name[4] = { "예금", "적금", "주택청약", "주식계좌" };
+    int bank[4] = { 5000, 4000, 0, 10 };
 
-			cout << endl << "===== 회원가입 성공 =====" << endl;
+    while (1) {
 
-		}
+        cout << endl;
+        cout << " ********** swing 은행 **********" << endl;
+        cout << "1. 회원가입 " << endl;
+        cout << "2. 로그인" << endl;
+        cout << "번호를 입력하세요 >> ";
+        cin >> a;
 
-		else if (a == 2) {
+        if (a == 1) {
+            cout << endl;
+            cout << "이름: ";
+            cin >> name;
+            cout << "아이디: ";
+            cin >> id;
+            cout << "비밀번호: ";
+            cin >> pw;
 
-			cout << "아이디: ";
-			cin >> loginid;
+            cout << endl << "===== 회원가입 성공 =====" << endl;
+            cout << endl;
 
-			cout << "비밀번호: ";
-			cin >> loginpw;
-			cout << endl;
+            continue;
+        }
+        else if (a == 2) {
+            cout << "아이디: ";
+            cin >> loginid;
 
-			if (loginpw == pw or loginid==id) {
-				cout << "===== 로그인 성공 =====";
-			}
+            cout << "비밀번호: ";
+            cin >> loginpw;
+            cout << endl;
 
-			else if (loginpw != pw and loginid != id) {
-				cout << "아이디 혹은 비밀번호가 틀렸습니다.";
-			}
-			
+            if (loginpw == pw or loginid == id) {
 
-		} 
+                is_logged_in = true;
+                cout << "===== 로그인 성공 =====";
+            }
+            else if (loginpw != pw and loginid != id) {
+
+                is_logged_in = false;
+                cout << "아이디 혹은 비밀번호가 틀렸습니다.";
+                continue; // 로그인 실패 시 다시 메뉴를 출력
+            }
+        }
+        cout << endl;
+
+        if (is_logged_in) {
+            cout << name << "님 환영합니다." << endl;
+            cout << endl;
+        }
+
+        break;
+    } 
+
+            while (1) {
+                cout << " **********swing 자산 관리 서비스 **********" << endl;
+                cout << "1. 계좌 정보 출력 " << endl;
+                cout << "2. 총 자산 출력" << endl;
+                cout << "3. 로그아웃 " << endl;
+
+                cout << "번호를 입력하세요 >>";
+                cin >> b;
+
+                if (b == 1) {
+                    myaccount(bank_name, bank);
+
+                    continue;
+                }
 
 
-					cout << endl;
+                else if (b == 2) {
 
-					cout << name << "님 환영합니다." << endl;
-					cout << endl;
+                    int sum = total(bank);
+                    cout << endl;
+                    cout << "현재 swing 자산관리 서비스에 등록된 회원님의 총 자산은 " << sum << "원 입니다." << endl;
+                    cout << endl;
 
-					while (1) {
-						cout << " * *********swing 자산 관리 서비스 **********" << endl;
-						cout << "1. 계좌 정보 출력 " << endl;
-						cout << "2. 총 자산 출력" << endl;
-						cout << "3. 로그아웃 " << endl;
+                    continue;
 
-						cout << "번호를 입력하세요 >>";
-						cin >> b;
+                }
 
-						if (b == 1) {
+                else if (b == 3) {
 
-							void myaccount(); {
+                    cout << endl << "===== 로그아웃 =====" << endl;
+                    cout << endl;
+                    cout << "로그아웃 되었습니다. 안녕히 가세요." << endl;
 
-								cout << "-------------------------- swing 계좌 정보 --------------------------" << endl;
-								cout << " " ;
+                    break;
+                }
 
+                return 0;
+            }
+        }
 
-							}
-						}
-
-						else if (b == 2) {
-
-							
-						}
-
-						else if (b == 3) {
-
-							cout << endl<< "===== 로그아웃 =====" << endl;
-							cout << endl;
-							cout << "로그아웃 되었습니다. 안녕히 가세요." << endl;
-
-							break;
-						}
-					} break;
-				}
-			}
-
-	
